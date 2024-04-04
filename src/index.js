e7f409a4dbd5a26b8270bdfc39a47c08ee2a2c62
Slicepop <kaden.seeger@gmail.com>
2024-04-04 16:37:26 -0400

    added icon pack and added if statements to listener. Add basic ui

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-addEventListener("keydown", (event) => {});
+addEventListener("keyup", (event) => {});
 
-onkeydown = (event) => {
-  console.log(event.code);
+onkeyup = (event) => {
+  if (event.code === "ArrowUp" || event.code === "KeyW") {
+    console.log(event.code);
+  } else if (event.code === "ArrowRight" || event.code === "KeyD") {
+    console.log(event.code);
+  } else if (event.code === "ArrowDown" || event.code === "KeyS") {
+    console.log(event.code);
+  } else if (event.code === "ArrowLeft" || event.code === "KeyA") {
+    console.log(event.code);
+  }
 };
