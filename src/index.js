da04e8bf10322b17242ba573b4ea1704238ef41d
Slicepop <kaden.seeger@gmail.com>
2024-04-08 11:58:38 -0400

    added link to icons in the stratagem list

--- src/index.js
+++ src/index.js
@@ -1,93 +1,310 @@
 // https://helldivers.fandom.com/wiki/Stratagem_Codes_(Helldivers_2)
 stratagems = [
-  [["down", "up", "up", "down", "up"], "LIFT-850 Jump Pack"],
-  [["down", "left", "down", "up", "up", "down"], "B-1 Supply Pack"],
+  [
+    ["down", "up", "up", "down", "up"],
+    "LIFT-850 Jump Pack",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Hangar/Jump Pack.svg",
+  ],
+  [
+    ["down", "left", "down", "up", "up", "down"],
+    "B-1 Supply Pack",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Supply Pack.svg",
+  ],
   [
     ["down", "up", "left", "up", "right", "right"],
     "AX/LAS-5 'Guard Dog' Rover",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Guard Dog Rover.svg",
   ],
   [
     ["down", "left", "down", "down", "up", "left"],
     "SH-20 Ballistic Shield Backpack",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Ballistic Shield Backpack.svg",
   ],
-  [["down", "up", "left", "up", "right", "down"], "AX/AR-23 'Guard Dog'"],
-  [["down", "left", "down", "up", "right"], "MG-43 Machine Gun"],
-  [["down", "left", "right", "up", "down"], "APW-1 Anti-Materiel Rifle"],
-  [["down", "left", "down", "up", "up", "left"], "M-105 Stalwart"],
-  [["down", "down", "left", "up", "right"], "EAT-17 Expendable Anti-tank"],
-  [["down", "left", "right", "right", "left"], "GR-8 Recoilless Rifle"],
-  [["down", "left", "up", "down", "up"], "FLAM-40 Flamethrower"],
-  [["down", "left", "down", "up", "up", "right"], "AC-8 Autocannon"],
-  [["down", "left", "up", "down", "down"], "MG-206 Heavy Machine Gun"],
-  [["down", "right", "down", "up", "left", "right"], "RS-422 Railgun"],
-  [["down", "down", "up", "down", "down"], "FAF-14 SPEAR Launcher"],
-  [["down", "left", "up", "left", "down"], "GL-21 Grenade Launcher"],
-  [["down", "left", "down", "up", "left"], "LAS-98 Laser Cannon"],
-  [["down", "right", "down", "up", "left", "right"], "ARC-3 Arc Thrower"],
-  [["down", "down", "up", "left", "right"], "LAS-99 Quasar Cannon"],
+  [
+    ["down", "up", "left", "up", "right", "down"],
+    "AX/AR-23 'Guard Dog'",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Robotics Workshop/Guard Dog.svg",
+  ],
+  [
+    ["down", "left", "down", "up", "right"],
+    "MG-43 Machine Gun",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Machine Gun.svg",
+  ],
+  [
+    ["down", "left", "right", "up", "down"],
+    "APW-1 Anti-Materiel Rifle",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Anti-Materiel Rifle.svg",
+  ],
+  [
+    ["down", "left", "down", "up", "up", "left"],
+    "M-105 Stalwart",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Stalwart.svg",
+  ],
+  [
+    ["down", "down", "left", "up", "right"],
+    "EAT-17 Expendable Anti-tank",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Expendable Anti-Tank.svg",
+  ],
+  [
+    ["down", "left", "right", "right", "left"],
+    "GR-8 Recoilless Rifle",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Recoilless Rifle.svg",
+  ],
+  [
+    ["down", "left", "up", "down", "up"],
+    "FLAM-40 Flamethrower",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Flamethrower.svg",
+  ],
+  [
+    ["down", "left", "down", "up", "up", "right"],
+    "AC-8 Autocannon",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Autocannon.svg",
+  ],
+  [
+    ["down", "left", "up", "down", "down"],
+    "MG-206 Heavy Machine Gun",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Heavy Machine Gun.svg",
+  ],
+  [
+    ["down", "right", "down", "up", "left", "right"],
+    "RS-422 Railgun",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Railgun.svg",
+  ],
+  [
+    ["down", "down", "up", "down", "down"],
+    "FAF-14 SPEAR Launcher",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Spear.svg",
+  ],
+  [
+    ["down", "left", "up", "left", "down"],
+    "GL-21 Grenade Launcher",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Grenade Launcher.svg",
+  ],
+  [
+    ["down", "left", "down", "up", "left"],
+    "LAS-98 Laser Cannon",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Laser Cannon.svg",
+  ],
+  [
+    ["down", "right", "down", "up", "left", "right"],
+    "ARC-3 Arc Thrower",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Arc Thrower.svg",
+  ],
+  [
+    ["down", "down", "up", "left", "right"],
+    "LAS-99 Quasar Cannon",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Quasar Cannon.svg",
+  ],
   [
     ["left", "down", "right", "up", "left", "down", "down"],
     "EXO-45 Patriot Exosuit",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Robotics Workshop/Patriot Exosuit.svg",
   ],
-  [["up", "down", "right", "left", "up"], "Reinforce"],
-  [["up", "down", "right", "up"], "SOS Beacon"],
-  [["down", "down", "up", "right"], "Resupply"],
+  [
+    ["up", "down", "right", "left", "up"],
+    "Reinforce",
+    "img/Helldivers-2-Stratagems-icons-svg-master/General Stratagems/Reinforce.svg",
+  ],
+  [
+    ["up", "down", "right", "up"],
+    "SOS Beacon",
+    "img/Helldivers-2-Stratagems-icons-svg-master/General Stratagems/SOS Beacon.svg",
+  ],
+  [
+    ["down", "down", "up", "right"],
+    "Resupply",
+    "img/Helldivers-2-Stratagems-icons-svg-master/General Stratagems/Resupply.svg",
+  ],
   [
     ["down", "up", "left", "down", "up", "right", "down", "up"],
     "NUX-223 Hellbomb",
+    "img/Helldivers-2-Stratagems-icons-svg-master/General Stratagems/Hellbomb.svg",
   ],
-  [["down", "down", "down", "up", "up"], "SSSD Delivery"],
-  [["up", "up", "left", "right", "down", "down"], "Seismic Probe"],
-  [["down", "down", "up", "up", "up"], "Upload Data"],
-  [["up", "up", "left", "up", "right"], "Eagle Rearm"],
-  //[["right", "right", "left", "left"], "Illumination Flare"],
-  [["right", "up", "up", "down"], "SEAF Artillery"],
-  [["down", "up", "down", "up"], "Super Earth Flag"],
+  [
+    ["down", "down", "down", "up", "up"],
+    "SSSD Delivery",
+    "img/Helldivers-2-Stratagems-icons-svg-master/General Stratagems/Upload Data.svg",
+  ],
+  [
+    ["up", "up", "left", "right", "down", "down"],
+    "Seismic Probe",
+    "img/Helldivers-2-Stratagems-icons-svg-master/General Stratagems/Seismic Probe.svg",
+  ],
+  [
+    ["down", "down", "up", "up", "up"],
+    "Upload Data",
+    "img/Helldivers-2-Stratagems-icons-svg-master/General Stratagems/Upload Data.svg",
+  ],
+  [
+    ["up", "up", "left", "up", "right"],
+    "Eagle Rearm",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Hangar/Eagle Rearm.svg",
+  ],
+  [
+    ["right", "right", "left", "left"],
+    "Illumination Flare",
+    "img/Helldivers-2-Stratagems-icons-svg-master/General Stratagems/Orbital Illumination Flare.svg",
+  ],
+  [
+    ["right", "up", "up", "down"],
+    "SEAF Artillery",
+    "img/Helldivers-2-Stratagems-icons-svg-master/General Stratagems/SEAF Artillery.svg",
+  ],
+  [
+    ["down", "up", "down", "up"],
+    "Super Earth Flag",
+    "img/Helldivers-2-Stratagems-icons-svg-master/General Stratagems/Super Earth Flag.svg",
+  ],
   [
     ["down", "up", "left", "right", "right", "left"],
     "E/MG-101 HMG Emplacement",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Bridge/HMG Emplacement.svg",
   ],
   [
     ["down", "down", "left", "right", "left", "right"],
     "FX-12 Shield Generator Relay",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Bridge/HMG Emplacement.svg",
   ],
-  [["down", "up", "right", "up", "left", "right"], "A/ARC-3 Tesla Tower"],
-  [["down", "left", "up", "right"], "MD-6 Anti-Personnel Minefield"],
-  [["down", "left", "left", "down"], "MD-I4 Incendiary Mines"],
-  [["down", "up", "right", "right", "up"], "A/MG-43 Machine Gun Sentry"],
-  [["down", "up", "right", "left"], "A/G-16 Gatling Sentry"],
-  [["down", "up", "right", "right", "down"], "A/M-12 Mortar Sentry"],
-  [["down", "up", "right", "up", "left", "up"], "A/AC-8 Autocannon Sentry"],
-  [["down", "up", "right", "left"], "A/MLS-4X Rocket Sentry"],
-  [["down", "up", "right", "down", "right"], "A/M-23 EMS Mortar Sentry"],
-  [["right", "down", "left", "up", "up"], "Orbital Gatling Barrage"],
-  [["right", "right", "right"], "Orbital Airburst  Barrage"],
+  [
+    ["down", "up", "right", "up", "left", "right"],
+    "A/ARC-3 Tesla Tower",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Bridge/Tesla Tower.svg",
+  ],
+  [
+    ["down", "left", "up", "right"],
+    "MD-6 Anti-Personnel Minefield",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Anti-Personnel Minefield.svg",
+  ],
+  [
+    ["down", "left", "left", "down"],
+    "MD-I4 Incendiary Mines",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Incendiary Mines.svg",
+  ],
+  [
+    ["down", "up", "right", "right", "up"],
+    "A/MG-43 Machine Gun Sentry",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Robotics Workshop/Machine Gun Sentry.svg",
+  ],
+  [
+    ["down", "up", "right", "left"],
+    "A/G-16 Gatling Sentry",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Robotics Workshop/Gatling Sentry.svg",
+  ],
+  [
+    ["down", "up", "right", "right", "down"],
+    "A/M-12 Mortar Sentry",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Robotics Workshop/Mortar Sentry.svg",
+  ],
+  [
+    ["down", "up", "right", "up", "left", "up"],
+    "A/AC-8 Autocannon Sentry",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Robotics Workshop/Autocannon Sentry.svg",
+  ],
+  [
+    ["down", "up", "right", "left"],
+    "A/MLS-4X Rocket Sentry",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Robotics Workshop/Rocket Sentry.svg",
+  ],
+  [
+    ["down", "up", "right", "down", "right"],
+    "A/M-23 EMS Mortar Sentry",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Robotics Workshop/EMS Mortar Sentry.svg",
+  ],
+  [
+    ["right", "down", "left", "up", "up"],
+    "Orbital Gatling Barrage",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Orbital Cannons/Orbital Gatling Barrage.svg",
+  ],
+  [
+    ["right", "right", "right"],
+    "Orbital Airburst  Barrage",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Orbital Cannons/Orbital Gatling Barrage.svg",
+  ],
   [
     ["right", "right", "down", "left", "right", "down"],
     "Orbital 120MM HE Barrage",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Orbital Cannons/Orbital 120MM HE Barrage.svg",
   ],
   [
     ["right", "down", "up", "up", "left", "down", "down"],
     "Orbital 380MM HE Barrage",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Orbital Cannons/Orbital 380MM HE Barrage.svg",
   ],
   [
     ["right", "down", "right", "down", "right", "down"],
     "Orbital Walking Barrage",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Orbital Cannons/Orbital Walking Barrage.svg",
   ],
-  [["right", "down", "up", "right", "down"], "Orbital Laser"],
-  [["right", "up", "down", "down", "right"], "Orbital Railcannon Strike"],
-  [["right", "right", "up"], "Orbital Precision Strike"],
-  [["right", "right", "down", "right"], "Orbital Gas Strike"],
-  [["right", "right", "left", "right"], "Orbital EMS Strike"],
-  [["right", "right", "down", "up"], "Orbital Smoke Strike"],
-  [["up", "right", "right"], "Eagle Strafing Run"],
-  [["up", "right", "down", "right"], "Eagle Airstrike"],
-  [["up", "right", "down", "down", "right"], "Eagle Cluster Bomb"],
-  [["up", "right", "down", "up"], "Eagle Napalm Airstrike"],
-  [["up", "right", "up", "down"], "Eagle Smoke Strike"],
-  [["up", "right", "up", "left"], "Eagle 110MM Rocket Pods"],
-  [["up", "right", "down", "down", "down"], "Eagle 500kg Bomb"],
-  [["down"], "You reached the end"], // remove this when finished
+  [
+    ["right", "down", "up", "right", "down"],
+    "Orbital Laser",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Orbital Cannons/Orbital Laser.svg",
+  ],
+  [
+    ["right", "up", "down", "down", "right"],
+    "Orbital Railcannon Strike",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Orbital Cannons/Orbital Railcannon Strike.svg",
+  ],
+  [
+    ["right", "right", "up"],
+    "Orbital Precision Strike",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Bridge/Orbital Precision Strike.svg",
+  ],
+  [
+    ["right", "right", "down", "right"],
+    "Orbital Gas Strike",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Bridge/Orbital Gas Strike.svg",
+  ],
+  [
+    ["right", "right", "left", "right"],
+    "Orbital EMS Strike",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Bridge/Orbital EMS Strike.svg",
+  ],
+  [
+    ["right", "right", "down", "up"],
+    "Orbital Smoke Strike",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Bridge/Orbital Smoke Strike.svg",
+  ],
+  [
+    ["up", "right", "right"],
+    "Eagle Strafing Run",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Hangar/Eagle Strafing Run.svg",
+  ],
+  [
+    ["up", "right", "down", "right"],
+    "Eagle Airstrike",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Hangar/Eagle Airstrike.svg",
+  ],
+  [
+    ["up", "right", "down", "down", "right"],
+    "Eagle Cluster Bomb",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Hangar/Eagle Cluster Bomb.svg",
+  ],
+  [
+    ["up", "right", "down", "up"],
+    "Eagle Napalm Airstrike",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Hangar/Eagle Napalm Airstrike.svg",
+  ],
+  [
+    ["up", "right", "up", "down"],
+    "Eagle Smoke Strike",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Hangar/Eagle Smoke Strike.svg",
+  ],
+  [
+    ["up", "right", "up", "left"],
+    "Eagle 110MM Rocket Pods",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Hangar/Eagle 110MM Rocket Pods.svg",
+  ],
+  [
+    ["up", "right", "down", "down", "down"],
+    "Eagle 500kg Bomb",
+    "img/Helldivers-2-Stratagems-icons-svg-master/Hangar/Eagle 500KG Bomb.svg",
+  ],
+  [["down"], "You reached the end", "img/down.png"], // remove this when finished
+  [["down"], "You reached the end", "img/down.png"], // remove this when finished
+  [["down"], "You reached the end", "img/down.png"], // remove this when finished
+  [["down"], "You reached the end", "img/down.png"], // remove this when finished
+  [["down"], "You reached the end", "img/down.png"], // remove this when finished
   //FIXME need to loop/tell me when I have finished
 ];
 
@@ -153,7 +370,19 @@ onkeyup = (event) => {
 
 //
 
+icon1 = document.querySelector(".icon1");
+icon2 = document.querySelector(".icon2");
+icon3 = document.querySelector(".icon3");
+icon4 = document.querySelector(".icon4");
+icon5 = document.querySelector(".icon5");
+
 function refreshStratagems() {
+  icon1.src = "../" + stratagems[stratagem_number][2];
+  icon2.src = "../" + stratagems[stratagem_number + 1][2];
+  icon3.src = "../" + stratagems[stratagem_number + 2][2];
+  icon4.src = "../" + stratagems[stratagem_number + 3][2];
+  icon5.src = "../" + stratagems[stratagem_number + 4][2];
+
   currentStratagem = stratagems[stratagem_number][0];
   current_command = 0;
   try {
